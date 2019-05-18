05239c0946f0237c31d689501eda4c9d2ddd50ae
Oliver <os@ventose.cc>
2019-05-18 19:55:56 +0200

    Benchmark data performance

--- src/ventose.cc/arduino/serial/serial_test.go
+++ src/ventose.cc/arduino/serial/serial_test.go
@@ -1,17 +1,18 @@
 package serial
 
 import (
-	"fmt"
 	"testing"
+	"time"
+
 	"github.com/jacobsa/go-serial/serial"
 )
 
 func TestSerialCommunication(t *testing.T) {
 	options := serial.OpenOptions{
-		PortName: "/dev/cu.usbmodem1423201",
-		BaudRate: 19200,
-		DataBits: 8,
-		StopBits: 1,
+		PortName:        "/dev/cu.usbmodem1423201",
+		BaudRate:        19200,
+		DataBits:        8,
+		StopBits:        1,
 		MinimumReadSize: 4,
 	}
 
@@ -21,9 +22,18 @@ func TestSerialCommunication(t *testing.T) {
 		panic(err)
 	}
 	defer port.Close()
-	b := make([]byte, 1)
-	for k := 0; k < 10000; k++ {
-		n, err  := port.Read(b);
-		fmt.Printf("[%d]n = %v err = %v b = %v\n", k, n, err, b)
+	for byteRate := 1; byteRate < 126; byteRate += 8 {
+		b := make([]byte, byteRate)
+		start := time.Now()
+		for k := 0; k < 3000; k++ {
+			_, err := port.Read(b)
+			if err != nil {
+				panic(err)
+			}
+			// go fmt.Printf("[%d]n = %v err = %v b = %v\n", k, n, err, b)
+		}
+		done := time.Now()
+		delta := done.Sub(start)
+		t.Logf("%f kb/s in %f s\n", float64(byteRate)/delta.Seconds(), delta.Seconds())
 	}
 }
